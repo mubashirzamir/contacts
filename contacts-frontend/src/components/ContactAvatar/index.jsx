8d9584d4c250c4c994e621f5f67b832c8d3ac56f
Mubashir Zamir <zamir.mubashir@gmail.com>
2025-02-21 02:43:04 +0000

    Avatar update.

--- contacts-frontend/src/components/ContactAvatar/index.jsx
+++ contacts-frontend/src/components/ContactAvatar/index.jsx
@@ -3,7 +3,11 @@ import {generateColor} from '@/helpers/utils.jsx'
 
 const UserAvatar = ({contact}) => {
     return <Avatar style={{backgroundColor: generateColor(contact.first_name)}} size={64}
-                   alt="Avatar">{contact?.first_name?.charAt(0)}</Avatar>
+                   alt="Avatar">
+        <div className="font-semibold text-xl">
+            {contact?.first_name?.charAt(0)}{contact?.surname?.charAt(0)}
+        </div>
+    </Avatar>
 }
 
 export default UserAvatar
