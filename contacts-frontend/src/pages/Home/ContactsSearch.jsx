441b96bc699239286a8df3089920a51a30bc1e01
Mubashir Zamir <zamir.mubashir@gmail.com>
2025-02-21 11:09:32 +0000

    allow clear.

--- contacts-frontend/src/pages/Home/ContactsSearch.jsx
+++ contacts-frontend/src/pages/Home/ContactsSearch.jsx
@@ -5,6 +5,7 @@ const ContactsSearch = ({search, setSearch}) => {
     return (
         <div className="contacts-search">
             <Input
+                allowClear
                 placeholder="Search contacts by name, email, or phone number."
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
